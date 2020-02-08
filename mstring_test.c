3e0846ef4bca7a09364a10fe7198c896e7d283b2
Kienyew <aobi100bt@hotmail.com>
2020-02-08 19:23:19 +0800

    formatted test logging.

--- mstring_test.c
+++ mstring_test.c
@@ -15,53 +15,81 @@ do {                                                                            
 } while (0)
 
 #define EXPECT_EQ_INT(expect, actual) EXPECT_EQ_BASE((expect) == (actual), (expect), (actual), "%d")
-#define EXPECT_EQ_SIZE(expect, actual) EXPECT_EQ_BASE((expect) == (actual), (expect), (actual), "%ld")
+#define EXPECT_EQ_SIZE(expect, actual) EXPECT_EQ_BASE((expect) == (actual), ((size_t)expect), (actual), "%ld")
 #define EXPECT_EQ_CHAR(expect, actual) EXPECT_EQ_BASE((expect) == (actual), (expect), (actual), "%c")
 #define EXPECT_EQ_STRING(expect, actual, actual_length) EXPECT_EQ_BASE((memcmp((expect), (actual), ((size_t)actual_length)) == 0), (expect), (actual), "%s")
-void test_string_init() {
-    string s;
-    string_init(&s, "test");
+void test_mstring_init() {
+    mstring s;
+    mstring_init(&s, "test");
     EXPECT_EQ_SIZE(4ul, s.size);
     EXPECT_EQ_STRING("test", s.base, 4);
-    string_free(&s);
+    mstring_free(&s);
 }
 
-void test_string_get() {
-    string s;
-    string_init(&s, "test");
-    EXPECT_EQ_STRING("test", string_get(&s), 4);
-    string_free(&s);
+void test_mstring_get() {
+    mstring s;
+    mstring_init(&s, "test");
+    EXPECT_EQ_STRING("test", mstring_get(&s), 4);
+    mstring_free(&s);
 }
 
-void test_string_append() {
-    string s;
-    string_init(&s, "");
-    string_append(&s, "append", 6);
-    EXPECT_EQ_STRING("append", string_get(&s), 6);
-    string_free(&s);
+void test_mstring_append() {
+    mstring s;
+    mstring_init(&s, "");
+    mstring_append(&s, "append", 6);
+    EXPECT_EQ_STRING("append", mstring_get(&s), 6);
+    mstring_free(&s);
 }
 
-void test_string_append_char() {
-    string s;
-    string_init(&s, "");
-    string_append_char(&s, 'a');
-    string_append_char(&s, 'o');
-    string_append_char(&s, 'b');
-    string_append_char(&s, 'i');
-    EXPECT_EQ_SIZE(4ul, s.size);
-    EXPECT_EQ_STRING("aobi", string_get(&s), 4);
-    string_free(&s);
+void test_mstring_append_char() {
+    mstring s;
+    mstring_init(&s, "");
+    mstring_append_char(&s, 'a');
+    mstring_append_char(&s, 'o');
+    mstring_append_char(&s, 'b');
+    mstring_append_char(&s, 'i');
+    EXPECT_EQ_SIZE(4, s.size);
+    EXPECT_EQ_STRING("aobi", mstring_get(&s), 4);
+    mstring_free(&s);
+}
+
+void test_mstring_get_size() {
+    mstring s;
+    mstring_init(&s, "");
+    EXPECT_EQ_SIZE(0, mstring_get_size(&s));
+    mstring_free(&s);
+
+    mstring_init(&s, "H");
+    EXPECT_EQ_SIZE(1, mstring_get_size(&s));
+    mstring_free(&s);
+
+    mstring_init(&s, "HELLO");
+    EXPECT_EQ_SIZE(5, mstring_get_size(&s));
+    mstring_free(&s);
+}
+
+void test_mstring_char_at() {
+    mstring s;
+    mstring_init(&s, "get_char");
+    EXPECT_EQ_CHAR('_', mstring_char_at(&s, 3));
+    mstring_free(&s);
+
+    mstring_init(&s, "c");
+    EXPECT_EQ_CHAR('c', mstring_char_at(&s, 0));
+    mstring_free(&s);
 }
 
 void test_all() {
-    test_string_init();
-    test_string_get();
-    test_string_append();
-    test_string_append_char();
+    test_mstring_init();
+    test_mstring_get();
+    test_mstring_append();
+    test_mstring_append_char();
+    test_mstring_get_size();
+    test_mstring_char_at();
 }
 
 int main() {
     test_all();
-    printf("Test (%d/%d), success rate: %.2f%%\n", test_success, test_total, (float)test_success/test_total);
+    printf("Test (%d/%d), success rate: %.2f%%\n", test_success, test_total, (float)test_success/test_total * 100.0f);
     return (test_total == test_success) ? 0 : 1;
 }
